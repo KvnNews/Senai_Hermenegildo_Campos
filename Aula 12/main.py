99f07d5169ff18f8890b18c64f3bf7f124220f03
Kevin N Silva <100316575+KvnNews@users.noreply.github.com>
2023-10-05 16:05:53 -0300

    Add files via upload

---
+++ Aula 12/main.py
@@ -0,0 +1,20 @@
+# soma = lambda x:x+3
+# print(soma(10))
+
+# lista = [1,2,3,6,2]
+# quadrado = list(map(lambda x: x**3, lista))
+# print(quadrado)
+
+
+# quadrado2 = list(map(lambda numero: numero + 10))
+# print(quadrado2)
+
+# multiplicacao = lambda a, b: a*b
+# print(multiplicacao(10,20))
+
+
+# Crie uma função lambda que retorne o dobro de um número.
+
+
+dobro = lambda x: x * 2
+print(dobro(5))
