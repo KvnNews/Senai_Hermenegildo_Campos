d862e5b1ffec6b96bfd1ca16993d3f7d120cb2a6
Kevin N Silva <100316575+KvnNews@users.noreply.github.com>
2023-10-18 16:55:50 -0300

    Add files via upload

---
+++ Aula 15/main.py
@@ -0,0 +1,9 @@
+# texto = '''Está é a primeira linha". 
+# Esta é a segunda linha.'''
+# print(texto
+      
+#       )
+
+from datetime import datetime
+date_atual = datetime.now()
+print(date_atual)
